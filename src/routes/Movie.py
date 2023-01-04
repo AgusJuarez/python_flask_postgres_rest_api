b8efa91eb87729b86d3c47c3b5fd91be3fca8ff2
AgusJuarez <agushjuarez@gmail.com>
2023-01-04 12:03:27 -0300

    Se agrega GET Movie y POST Add movie (Falta terminar)

--- src/routes/Movie.py
+++ src/routes/Movie.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 #Models
 from models.MovieModel import MovieModel
 
@@ -12,3 +12,23 @@ def get_movies():
     except Exception as ex:
         return jsonify({'message':str(ex)}),500
     
+@main.route('/<id>')
+def get_movie(id):
+    try:
+        movie = MovieModel.get_movie(id)
+        if movie != None:
+            return jsonify(movie)
+        else:
+            return jsonify({}), 404
+    except Exception as ex:
+        return jsonify({'message':str(ex)}),500
+
+@main.route('/add', methods=['POST'])
+def add_movie():
+    try:
+        print(request.json)
+        return jsonify({})
+    except Exception as ex:
+        return jsonify({'message':str(ex)}),500
+
+
